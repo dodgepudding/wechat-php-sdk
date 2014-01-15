4f163b95ce55131b2974f5dddb8286c746b626fa
neway3000 <neway3000@qq.com>
2014-01-14 17:47:04 -0800

    增加获得用户头像数据接口

--- wechatext.class.php
+++ wechatext.class.php
@@ -404,6 +404,28 @@ class Wechatext
 		}
 		return false;
 	}
+	
+	
+	/**
+	 * 获得头像数据
+	 *
+	 * @param FakeId $fakeid
+	 * @return JPG二进制数据
+	 */
+	public function getHeadImg($fakeid){
+		$send_snoopy = new Snoopy; 
+		$send_snoopy->rawheaders['Cookie']= $this->cookie;
+		$send_snoopy->referer = "https://mp.weixin.qq.com/cgi-bin/getmessage?t=wxm-message&lang=zh_CN&count=50&token=".$this->_token;
+		$url = "https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=$fakeid&token=".$this->_token."&lang=zh_CN";
+		$send_snoopy->fetch($url);
+		$result = $send_snoopy->results;
+		$this->log('Head image:'.$fakeid.'; length:'.strlen($result));
+		if(!$result){
+			return false;
+		}
+		return $result;
+	}
+	
 
 	/**
 	 * 获取消息更新数目
