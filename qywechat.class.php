e9794c98de5e545bb51ac0cf7ead95e6d87229f2
lscho <me@lscho.com>
2017-06-26 16:57:08 +0800

    修复企业微信获取不到access_token问题

--- qywechat.class.php
+++ qywechat.class.php
@@ -894,7 +894,7 @@ class Wechat
 		if ($result)
 		{
 			$json = json_decode($result,true);
-			if (!$json || isset($json['errcode'])) {
+			if (!$json || !empty($json['errcode'])) {
 				$this->errCode = $json['errcode'];
 				$this->errMsg = $json['errmsg'];
 				return false;
